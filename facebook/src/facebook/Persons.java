2ffaa3ee2bf55475a81fb6a978c912ff6066c037
HM12341234 <aloshalhamoud@gmail.com>
2024-12-09 17:32:50 +0200

    Update Persons.java

--- facebook/src/facebook/Persons.java
+++ facebook/src/facebook/Persons.java
@@ -20,9 +20,9 @@ public class Persons {
 
         Person p = new Person (
                 new ArrayList<>(List.of(
-                        "1.1|like:12|security:private[2,3]tag:yes[2]{WHO AM I @?",
+                        "1.1|like:12|private{WHO AM I @mona_2",
                         "1.1.1|like:3{you are Hisham",
-                        "1.2|like:5|security:public{I like coffee",
+                        "1.2|like:5|public{I like coffee",
                         "1.2.1|like:2{and me !"
 
                 )), "Mohame Ali", 1 , "12/2/2000", "male" , "Mohamed_Ali@gmail.com", "12345678" , new ArrayList<>(List.of(1,2,3)));
@@ -32,7 +32,7 @@ public class Persons {
 
         p = new Person (
                 new ArrayList<>(List.of(
-                        "1.1|like:12|security:private[2,3]tag:yes[2]{WHO AM I @?",
+                        "1.1|like:12|private{WHO AM I @Mohamed_1",
                         "2.1.1|like:3{you are Hisham",
                         "2.2|like:5|security:public{I like coffee",
                         "2.2.1|like:2{and me !"
@@ -45,10 +45,10 @@ public class Persons {
 
         p = new Person (
                 new ArrayList<>(List.of(
-                        "1.1|like:12|security:private[2,3]tag:yes[2]{WHO AM I @?",
-                        "3.1.1|like:3{you are Hisham",
-                        "3.2|like:5|security:public{I like coffee",
-                        "3.2.1|like:2{and me !"
+                         "1.1|like:12|private{WHO AM I @mona_2",
+                        "1.1.1|like:3{you are Hisham",
+                        "1.2|like:5|public{I like coffee",
+                        "1.2.1|like:2{and me !"
 
                 )), "Ahmed yaser", 3 , "12/2/2000", "male" , "ahmed@gmail.com", "12345678",  new ArrayList<>(List.of(1,2,3)) );
 
