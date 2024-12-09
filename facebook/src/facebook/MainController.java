b44cfee2b03748c7a6d2b044f2651f6a0ae07f16
HM12341234 <aloshalhamoud@gmail.com>
2024-12-09 17:28:33 +0200

    Update MainController.java

--- facebook/src/facebook/MainController.java
+++ facebook/src/facebook/MainController.java
@@ -1,27 +1,34 @@
 package facebook; 
 
+import javafx.fxml.FXML;
+
 
 public class MainController {
    
     
     
-    
+   @FXML
    public void profile () {}
-   
+   @FXML
    public void chats() {}
-    
+   @FXML
    public void write_post(){
    
-     WritePostController write = new WritePostController () ; 
+     WritePostController write = new WritePostController () ;
      write.write_post();
    
  
     }
-   
+   @FXML
    public void friends_posts () {} 
+   @FXML
+   public void all_posts () {
    
-   public void all_posts () {}
    
+   
+   
+   }
+   @FXML
    public void search () {}
    
    
